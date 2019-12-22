0466ae6a435f00b0d0a03bbd1339f0997e2d046d
zy <1193398590@qq.com>
2019-12-22 19:08:29 +0800

    经纬度

--- src/main/java/com/zhaoya/common/untils/DistanceUtil.java
+++ src/main/java/com/zhaoya/common/untils/DistanceUtil.java
@@ -4,17 +4,19 @@ import java.awt.geom.Point2D;
 
 public class DistanceUtil {
 
-	private static final double EARTH_RADIUS = 6371393;
-
+	private static final double EARTH_RADIUS = 6371393;//平均半径 单位：m
+    //输入经纬度，计算两个坐标间的距离
 	public static double getDistance(double j1, double w1, double j2, double w2) {
 		Point2D pointDD = new Point2D.Double(j1, w1);
 		Point2D pointXD = new Point2D.Double(j2, w2);
 		return calculate(pointDD, pointXD);
 	}
 
+	//通过AB点经纬度获取距离
 	public static double calculate(Point2D pointA, Point2D pointB) {
 
-		double radiansAX = Math.toRadians(pointA.getX());
+		//经纬度(角度)转弧度，弧度用作参数,以调用Math.cos和Math.sin
+		double radiansAX = Math.toRadians(pointA.getX());//A经弧度
 		double radiansAY = Math.toRadians(pointA.getY());
 		double radiansBX = Math.toRadians(pointB.getX());
 		double radiansBY = Math.toRadians(pointB.getY());
