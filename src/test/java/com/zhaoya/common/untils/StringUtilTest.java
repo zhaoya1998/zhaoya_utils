35d321b99d98c8abcb365b9d723ee2464d0d763b
zy <1193398590@qq.com>
2020-01-03 15:09:36 +0800

    工具类

--- src/test/java/com/zhaoya/common/untils/StringUtilTest.java
+++ src/test/java/com/zhaoya/common/untils/StringUtilTest.java
@@ -1,5 +1,7 @@
 package com.zhaoya.common.untils;
 
+import java.util.Calendar;
+
 import org.junit.Test;
 
 public class StringUtilTest {
@@ -63,5 +65,27 @@ public class StringUtilTest {
 		String url="http://news.cnstock.com/news,yw-201908-4413224.htm";
 		String string = StringUtil.getLastNumber(url);
 		System.out.println(string);
+	}
+	
+	@Test
+	public void testHashTest() {
+		// String src="abc";
+		String src = "";
+		boolean b = StringUtil.hasText(src);
+		if (b) {
+			System.out.println("有值得");
+		} else {
+			System.out.println("无值得");
+		}
+	}
+
+	@Test
+	public void testGetAge() {
+		// 成功
+		Calendar c = Calendar.getInstance();
+		c.set(1997, 7, 2);
+		int age = DateUtil.getAge(c.getTime());
+		System.out.println(age);
+
 	}
 }
